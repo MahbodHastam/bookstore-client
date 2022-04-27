738789b2e2ee211064fafa812accdf1d321b0d0f
MahbodHastam <mahbod4782@gmail.com>
2022-04-28 03:19:42 +0430

    finished header navbar

---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <a href="/" v-if="text" class="font-lalezar text-3xl text-indigo-700 relative">
+    <BookOpenIcon
+      class="h-12 w-12 inline -translate-y-1 bg-indigo-100 px-2 rounded-lg ml-2"
+    />
+    کتابستان
+  </a> 
+  <a href="/" v-else class="font-lalezar text-3xl text-indigo-700 relative">
+    <BookOpenIcon
+      class="h-12 w-12 inline -translate-y-1 bg-indigo-100 px-2 rounded-lg ml-2"
+    />
+  </a>
+</template>
+
+<script setup>
+import { BookOpenIcon } from '@heroicons/vue/solid'
+
+defineProps({
+  text: Boolean,
+})
+</script>
