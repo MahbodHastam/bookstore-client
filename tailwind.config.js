3aee1ff9e0fc28ea5224644bbe40c37667d56ac8
MahbodHastam <mahbod4782@gmail.com>
2022-04-28 03:09:51 +0430

    define new fonts into tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Estedad', ...defaultTheme.fontFamily.sans],
+        lalezar: 'Lalezar',
+      },
+    },
   },
   plugins: [
     require('tailwindcss-rtl'),
