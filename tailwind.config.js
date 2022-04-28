f155f86870a1794e7e3f2fb3e82f509f5eff61e8
MahbodHastam <mahbod4782@gmail.com>
2022-04-29 01:23:19 +0430

    changed primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
     extend: {
       fontFamily: {
         sans: ['Estedad', ...defaultTheme.fontFamily.sans],
         lalezar: 'Lalezar',
+      },
+      colors: {
+        primary: colors.blue,
       },
     },
   },
