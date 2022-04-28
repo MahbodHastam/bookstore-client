b59ac55086607c4d4322a017e7648ad6abbaa7e8
MahbodHastam <mahbod4782@gmail.com>
2022-04-29 01:27:48 +0430

    added UiButton to autoload

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, h } from 'vue'
 import 'boxicons/css/boxicons.min.css'
 import './index.css'
 import App from './App.vue'
 import { router } from './router'
+import UiButton from './components/UiButton.vue'
 
 const app = createApp(App)
+
+app.component('ui-button', UiButton)
 
 app.use(router)
 
