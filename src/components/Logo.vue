53d21a16ae85741c88b323f022d4a82d70627642
MahbodHastam <mahbod4782@gmail.com>
2022-04-29 01:24:09 +0430

    added another rule changed primary color

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,14 +1,17 @@
 <template>
-  <a href="/" v-if="text" class="font-lalezar text-3xl text-indigo-700 relative">
+  <a href="/" v-if="text" class="font-lalezar text-3xl text-primary-700 relative">
     <BookOpenIcon
-      class="h-12 w-12 inline -translate-y-1 bg-indigo-100 px-2 rounded-lg ml-2"
+      class="h-12 w-12 inline -translate-y-1 bg-primary-100 px-2 rounded-lg ml-2"
     />
-    کتابستان
+    کتابستانـ
   </a> 
-  <a href="/" v-else class="font-lalezar text-3xl text-indigo-700 relative">
+  <a href="/" v-if="!text && !textOnly" class="font-lalezar text-3xl text-primary-700 relative">
     <BookOpenIcon
-      class="h-12 w-12 inline -translate-y-1 bg-indigo-100 px-2 rounded-lg ml-2"
+      class="h-12 w-12 inline -translate-y-1 bg-primary-100 px-2 rounded-lg ml-2"
     />
+  </a>
+  <a href="/" v-if="textOnly" :class="textOnly" v-bind="$attrs" class="font-lalezar text-primary-700">
+    کتابستانـ
   </a>
 </template>
 
@@ -17,5 +20,11 @@ import { BookOpenIcon } from '@heroicons/vue/solid'
 
 defineProps({
   text: Boolean,
+  textOnly: {
+    type: [Boolean, String],
+    default() {
+      return false
+    }
+  },
 })
 </script>
