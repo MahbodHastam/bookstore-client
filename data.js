1081f48f07725224b82404bc8ecaa33c116b8c2a
MahbodHastam <mahbod4782@gmail.com>
2022-05-02 01:42:32 +0430

    new things with full-power of bugs

---
+++ data.js
@@ -0,0 +1,16 @@
+export const popularBooksRaw = [
+  { title: 'علی پسری در مزرعه', cover: '', free: true },
+  { title: 'گوربه‌ای میان درختان', cover: '', free: true },
+  { title: 'گودرت خاک', cover: '', free: false },
+  { title: 'عمو پورنگ و خانه‌ی ما', cover: '', free: false },
+]
+
+export const newestBooksRaw = [
+  { title: 'علی پسری در مزرعه', cover: '', free: true },
+  { title: 'عمو پورنگ و خانه‌ی ما', cover: '', free: false },
+]
+
+export const nonFreeBooksRaw = [
+  { title: 'گودرت خاک', cover: '', free: false },
+  { title: 'عمو پورنگ و خانه‌ی ما', cover: '', free: false },
+]
--- src/components/BooksSwiper.vue
+++ src/components/BooksSwiper.vue
@@ -1,21 +1,54 @@
 <template>
   <div>
-    <h4 class="text-2xl text-primary-700">
-      <BookOpenIcon class="w-6 h-6" />
-      {{ headingTitle }}
+    <h4 class="text-4xl font-bold text-primary-600 text-center space-s-4">
+      <HeartIcon
+        :class="headingTitleIconClassNames"
+        v-if="type === 'popular'"
+      />
+      <SparklesIcon
+        :class="headingTitleIconClassNames"
+        v-else-if="type === 'newest'"
+      />
+      <KeyIcon
+        :class="headingTitleIconClassNames"
+        v-else-if="type === 'non-free'"
+      />
+      <BookOpenIcon :class="headingTitleIconClassNames" v-else />
+      <span>
+        {{ headingTitle }}
+      </span>
     </h4>
+    swiper
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { BookOpenIcon } from '@heroicons/vue/outline'
+import { HeartIcon, SparklesIcon, KeyIcon } from '@heroicons/vue/solid'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { popularBooksRaw, newestBooksRaw, nonFreeBooksRaw } from '../../data'
+
+const headingTitleIconClassNames = `w-12 h-12 inline-block -mt-2`
 
 defineProps({
-  headingTitle: String,
+  headingTitle: {
+    type: [null, String],
+    default: 'کتاب‌های اخیر',
+  },
   type: {
     validator(value) {
       return ['popular', 'newest', 'non-free'].includes(value)
-    }
+    },
+    default: 'popular',
   },
 })
+
+function getBooks(neededType) {
+  if (neededType === 'pupolar') return popularBooksRaw
+  if (neededType === 'newest') return newestBooksRaw
+  if (neededType === 'non-free') return nonFreeBooksRaw
+}
+
+const books = getBooks(type)
 </script>
