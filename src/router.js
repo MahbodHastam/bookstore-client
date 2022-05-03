24755c9ead4eb0bbc593b1c14dc3f9d868e96b33
MahbodHastam <mahbod4782@gmail.com>
2022-05-03 12:25:34 +0430

    added new pages and routes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 export const routes = [
-  { path: '/', component: () => import('./pages/index.vue') }
+  { path: '/', component: () => import('./pages/index.vue') },
+  { path: '/about', component: () => import('./pages/about.vue') },
+  { path: '/features', component: () => import('./pages/features.vue') },
 ]
 
 export const router = createRouter({
