6f78128fb84f1105a09a3bdd8467f7df5f3d473d
MahbodHastam <mahbod4782@gmail.com>
2022-05-04 01:23:43 +0430

    added pages transitions + swiper css bundle

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
+  <div>
+    <header-nav />
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in" appear>
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <script setup>
+import 'swiper/css/bundle'
+import HeaderNav from './components/HeaderNav.vue'
 </script>
 
 <style>
 /* #app {
   color: #2c3e50;
 } */
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(100px)
+}
+.route-enter-active,
+.route-leave-active {
+  transition: all .2s ease-out
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(100px)
+}
 </style>
