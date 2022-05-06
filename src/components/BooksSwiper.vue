c57f349c9c28291e1015339d4269d60942ffa4ea
MahbodHastam <mahbod4782@gmail.com>
2022-05-06 15:47:44 +0430

    reformat code

--- src/components/BooksSwiper.vue
+++ src/components/BooksSwiper.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="mb-12">
     <h4 class="text-4xl font-bold text-primary-600 text-center space-s-4 mb-12">
       <HeartIcon
         :class="headingTitleIconClassNames"
@@ -27,35 +27,134 @@
       class="!py-20 !px-3"
     >
       <swiper-slide v-for="(book, idx) in books" :key="idx">
-        <div class="bg-gradient-to-b from-slate-200 via-slate-100 to-white shadow-xl shadow-gray-300 rounded-lg p-4 max-w-md mx-auto group flex justify-center flex-col">
-          <div class="relative transition-all rounded-lg -mt-14 overflow-hidden w-11/12 h-64 shadow-gray-300 shadow-[0_-10px_15px_-9px,0_4px_15px_3px] group-hover:scale-105 self-center">
-            <img :src="book.cover" :alt="book.title + ' cover'" class="rounded-lg w-full h-64 transition-all duration-500 group-hover:scale-105">
-            <div class="absolute h-12 w-full gap-2 flex items-center justify-end flex-row bottom-3 left-3">
-              <a href="#" class="rounded-full p-3 bg-green-600  text-white bg-opacity-80 hover:bg-opacity-100 transition-all">
+        <div
+          class="
+            bg-gradient-to-b
+            from-slate-200
+            via-slate-100
+            to-white
+            shadow-xl shadow-gray-300
+            rounded-lg
+            p-4
+            max-w-md
+            mx-auto
+            group
+            flex
+            justify-center
+            flex-col
+          "
+        >
+          <div
+            class="
+              relative
+              transition-all
+              rounded-lg
+              -mt-14
+              overflow-hidden
+              w-11/12
+              h-64
+              shadow-gray-300
+              shadow-[0_-10px_15px_-9px,0_4px_15px_3px]
+              group-hover:scale-105
+              self-center
+            "
+          >
+            <img
+              :src="book.cover"
+              :alt="book.title + ' cover'"
+              class="
+                rounded-lg
+                w-full
+                h-64
+                transition-all
+                duration-500
+                group-hover:scale-105
+              "
+            />
+            <div
+              class="
+                absolute
+                h-12
+                w-full
+                gap-2
+                flex
+                items-center
+                justify-end
+                flex-row
+                bottom-3
+                left-3
+              "
+            >
+              <a
+                href="#"
+                class="
+                  rounded-full
+                  p-3
+                  bg-green-600
+                  text-white
+                  bg-opacity-80
+                  hover:bg-opacity-100
+                  transition-all
+                "
+              >
                 <UserGroupIcon class="w-4" />
               </a>
-              <a href="#" class="text-sm space-s-1 px-4 py-2 rounded-full transition-all bg-primary-600 text-primary-100 bg-opacity-95 hover:bg-opacity-100 hover:text-primary-50">
+              <a
+                href="#"
+                class="
+                  text-sm
+                  space-s-1
+                  px-4
+                  py-2
+                  rounded-full
+                  transition-all
+                  bg-primary-600
+                  text-primary-100
+                  bg-opacity-95
+                  hover:bg-opacity-100 hover:text-primary-50
+                "
+              >
                 <EyeIcon class="w-4 inline-block" />
-                <span>
-                  شروع به خواندن
-                </span>
+                <span> شروع به خواندن </span>
               </a>
             </div>
           </div>
           <div class="py-3 px-4">
-            <a href="#" class="text-xl text-slate-800 hover:text-slate-600 transition-colors">
+            <a
+              href="#"
+              class="
+                text-xl text-slate-800
+                hover:text-slate-600
+                transition-colors
+              "
+            >
               {{ book.title }}
             </a>
             <div class="flex flex-row justify-around items-center mt-6">
-              <span class="rounded-full bg-cyan-100 text-cyan-600 px-3 py-1 text-sm">
+              <span
+                class="rounded-full bg-cyan-100 text-cyan-600 px-3 py-1 text-sm"
+              >
                 <i class="bx bx-book"></i>
                 {{ book.attributes.pages }} صفحه
               </span>
-              <span v-if="book.free" class="rounded-full bg-lime-100 text-lime-600 px-3 py-1 text-sm">
+              <span
+                v-if="book.free"
+                class="rounded-full bg-lime-100 text-lime-600 px-3 py-1 text-sm"
+              >
                 <i class="bx bx-purchase-tag"></i>
                 رایگان
               </span>
-              <span v-else class="rounded-full bg-amber-100 text-amber-600 px-3 py-1 text-sm">
+              <span
+                v-else
+                class="
+                  rounded-full
+                  bg-amber-100
+                  text-amber-600
+                  px-3
+                  py-1
+                  text-sm
+                "
+              >
                 <i class="bx bx-lock"></i>
                 نیازمند حساب
               </span>
@@ -64,17 +163,18 @@
         </div>
       </swiper-slide>
     </swiper>
-    <br>
-    <br>
-    <br>
-    <br>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { BookOpenIcon, EyeIcon } from '@heroicons/vue/outline'
-import { HeartIcon, SparklesIcon, KeyIcon, UserGroupIcon } from '@heroicons/vue/solid'
+import {
+  HeartIcon,
+  SparklesIcon,
+  KeyIcon,
+  UserGroupIcon,
+} from '@heroicons/vue/solid'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/vue'
 import { Autoplay } from 'swiper'
 import Data, { popularBooksRaw, newestBooksRaw, nonFreeBooksRaw } from '../data'
