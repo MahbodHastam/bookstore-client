037e13a833d02641f9737de01cad31c6b4528a0c
MahbodHastam <mahbod4782@gmail.com>
2022-05-06 19:36:08 +0430

    remove dummy texts

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,12 @@
 <template>
-  <div>
-    about page
+  <div class="pb-96 container mx-auto">
+    <h4 class="text-slate-700 text-2xl">
+      <book-open-icon class="w-6 h-6 inline-block me-2" />
+      صفحه درباره کتابستان (ناتمام)
+    </h4>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { BookOpenIcon } from '@heroicons/vue/outline'
+</script>
