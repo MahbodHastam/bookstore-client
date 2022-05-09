2913d10532881ed094815ca93e4caa6f1cc9640f
MahbodHastam <mahbod4782@gmail.com>
2022-05-10 00:33:45 +0430

    changed about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,12 +1,54 @@
 <template>
-  <div class="pb-96 container mx-auto">
-    <h4 class="text-slate-700 text-2xl">
-      <book-open-icon class="w-6 h-6 inline-block me-2" />
-      صفحه درباره کتابستان (ناتمام)
+  <div class="pb-24 container mx-auto text-center">
+    <h4 class="text-slate-800 text-3xl font-medium pb-0">
+      در حال حاظر...
+    </h4>
+    <div class="grid grid-cols-1 md:grid-cols-4 my-16 gap-12">
+
+      <div class="flex flex-col items-center">
+        <users-icon class="w-14 block text-slate-800 pb-2" />
+        <span class="block text-2xl text-slate-900 font-bold animate-pulse">
+          {{ '۱۲۳۰' }}
+        </span>
+        <span class="block text-lg text-slate-700">
+          عضو عادی
+        </span>
+      </div>
+      <div class="flex flex-col items-center">
+        <star-icon class="w-14 block text-slate-800 pb-2" />
+        <span class="block text-2xl text-slate-900 font-bold animate-pulse">
+          {{ '۱۷۳' }}
+        </span>
+        <span class="block text-lg text-slate-700">
+          عضو ویژه
+        </span>
+      </div>
+      <div class="flex flex-col items-center">
+        <book-open-icon class="w-14 block text-slate-800 pb-2" />
+        <span class="block text-2xl text-slate-900 font-bold animate-pulse">
+          {{ '۶' }}
+        </span>
+        <span class="block text-lg text-slate-700">
+          کتاب رایگان
+        </span>
+      </div>
+      <div class="flex flex-col items-center">
+        <sparkles-icon class="w-14 block text-slate-800 pb-2" />
+        <span class="block text-2xl text-slate-900 font-bold animate-pulse">
+          {{ '۳' }}
+        </span>
+        <span class="block text-lg text-slate-700">
+          کتاب ویژه
+        </span>
+      </div>
+
+    </div>
+    <h4 class="text-slate-800 text-3xl font-medium">
+      در <span class="font-lalezar">کتابستانـ</span>، فعال/موجود هستند.
     </h4>
   </div>
 </template>
 
 <script setup>
-import { BookOpenIcon } from '@heroicons/vue/outline'
+import { UsersIcon, StarIcon, BookOpenIcon, SparklesIcon } from '@heroicons/vue/solid'
 </script>
